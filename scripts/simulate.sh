b005ed4763a0cb0fccbf6832ca9463264e9a7f12
pedro-fuoco <pedrofuoco6@gmail.com>
2022-05-31 11:51:59 -0300

    simulation script update

--- scripts/simulate.sh
+++ scripts/simulate.sh
@@ -1,7 +1,7 @@
-SIMULATION_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd )"
+SIMULATION_DIR="$HOME/skyrats-workplace/src/PX4-Autopilot"
 FIRMWARE_DIR="$HOME/skyrats-workplace/src/PX4-Autopilot"
 FIRMWARE_BUILD_DIR="$HOME/skyrats-workplace/src/PX4-Autopilot/build/px4_sitl_default"
-
+source /usr/share/gazebo/setup.sh
 # Types of drones
 #declare -a TYPES_OF_DRONES
 #TYPES_OF_DRONES="ropped_iris ropped_swarm iris iris_depth_camera iris_downward_depth_camera iris_fpv_cam iris_rplidar iris_stereo_camera"
@@ -17,14 +17,28 @@ export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$FIRMWARE_DIR/Tools/sitl_gazebo
 
 # Setup Gazebo to find this package's models and plugins
 export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${SIMULATION_DIR}/models
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:${SIMULATION_DIR}/../../devel/lib
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:${SIMULATION_DIR}/build/px4_sitl_default/build_gazebo
 
 # Setup skyrats interface for gazebo
  > ~/.gazebo/gui.ini
 echo -e "[geometry]\nx=0\ny=0\n[overlay_plugins]\nfilenames=libskyrats_interface.so" >> ~/.gazebo/gui.ini
 
 
+#cd $HOME/skyrats-workplace/src/PX4-Autopilot/Tools/sitl_gazebo/worlds
+#gazebo aruco.world --verbose
+
+
 cd $HOME/skyrats-workplace/src/PX4-Autopilot
 make px4_sitl gazebo_iris_fpv_cam
 
 
+##future ideas##
+
+#SIM SPEED CONTROL
+##export PX4_SIM_SPEED_FACTOR=2
+
+#Different models and worlds
+##make px4_sitl gazebo_iris_irlock__sonoma_raceway
+
+#Verbose mode
+##export VERBOSE_SIM=1
