d5ff5d1eb2ac46ebe8058acd6520bee19dabe543
pedro-fuoco <pedrofuoco6@gmail.com>
2022-06-13 12:57:06 -0300

    independent package from sitl_gazebo

--- scripts/simulate.sh
+++ scripts/simulate.sh
@@ -1,11 +1,8 @@
-SIMULATION_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")/../../../install/simulation2/share/simulation2" && pwd )"
+SIMULATION_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd )"
 FIRMWARE_DIR="$HOME/skyrats-workplace/src/PX4-Autopilot"
 FIRMWARE_BUILD_DIR="$HOME/skyrats-workplace/src/PX4-Autopilot/build/px4_sitl_default"
 
 source /usr/share/gazebo/setup.sh
-# Types of drones
-#declare -a TYPES_OF_DRONES
-#TYPES_OF_DRONES="ropped_iris ropped_swarm iris iris_depth_camera iris_downward_depth_camera iris_fpv_cam iris_rplidar iris_stereo_camera"
 
 cd $FIRMWARE_DIR 
 
@@ -18,10 +15,10 @@ export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$FIRMWARE_DIR/Tools/sitl_gazebo
 
 # Setup Gazebo to find this package's models and plugins
 export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${SIMULATION_DIR}/models
-echo AAAAAAAAAAAAAAAA
-echo $GAZEBO_MODEL_PATH
-echo AAAAAAAAAAAAAAAA
 export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:${FIRMWARE_DIR}/build/px4_sitl_default/build_gazebo
+echo AAAAAAAAAAAAAAAAAAA
+echo $GAZEBO_PLUGIN_PATH
+echo AAAAAAAAAAAAAAAAAAA
 
 #cd $HOME/skyrats_ws2/install/simulation2/share/simulation2/worlds
 #gazebo aruco.world --verbose
@@ -30,7 +27,8 @@ export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:${FIRMWARE_DIR}/build/px4_sitl_d
 #cd $HOME/skyrats-workplace/src/PX4-Autopilot
 #make px4_sitl gazebo
 
-ros2 launch simulation2 simulate.launch.py world:='~/skyrats_ws2/src/simulation2/worlds/aruco.world' drone:="$HOME/skyrats_ws2/install/simulation2/share/simulation2/models/iris_fpv_cam_downward/iris_fpv_cam_downward.sdf"
+#ros2 launch simulation2 simulate.launch.py world:="$SIMULATION_DIR/worlds/aruco.world" drone:="$SIMULATION_DIR/models/iris_fpv_cam_downward/iris_fpv_cam_downward.sdf"
+ros2 launch simulation2 simulate.launch.py world:="$SIMULATION_DIR/worlds/aruco.world" drone:="$SIMULATION_DIR/models/iris_green_house/iris_green_house.sdf"
 
 ##future ideas##
 
